7294fe87456453d96364b93f1135bb8f24c62a74
Fiskgeo <rncrus@gmail.com>
2025-10-07 15:16:09 +0300

    07-10-25

---
+++ lesson2/02_lesson/lesson_2_task_4.py
@@ -0,0 +1,16 @@
+from errno import EILSEQ
+
+
+def fizz_buzz(x):
+    for x in range(1, x + 1):
+        if x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+                print("Fizz")
+        elif x % 5 == 0:
+                        print("Buzz")
+        else:
+           print(x)
+
+num=int(input("Введите число: "))
+fizz_buzz(num)
