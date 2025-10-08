908b212ed5646839d7a713b436c95ea8b27643ac
Fiskgeo <rncrus@gmail.com>
2025-10-08 12:06:47 +0300

    07-10-25

--- lesson3/03_lesson/lesson_3_task_1.py
+++ lesson3/03_lesson/lesson_3_task_1.py
@@ -1,2 +1,10 @@
-import User from user
+from user import User
+
+
+my_user = User("Yury", "Volkov")
+
+print(my_user.first_name)
+print(my_user.last_name)
+
+print((f"name: {my_user.first_name} {my_user.last_name}"))
 
