ca38bf1f01c96bc0a9b30e6f680b5522c9b957fc
Fiskgeo <rncrus@gmail.com>
2025-10-16 11:18:44 +0300

    07-10-25

---
+++ lesson5/05_lesson/lesson05_task1.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/classattr")
+sleep(15)
