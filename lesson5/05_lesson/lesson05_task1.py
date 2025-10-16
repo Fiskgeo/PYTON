deb4b1adfc13d889859a0d5b0e392fdc4d2a9dbf
Fiskgeo <rncrus@gmail.com>
2025-10-16 13:14:32 +0300

    07-10-25

--- lesson5/05_lesson/lesson05_task1.py
+++ lesson5/05_lesson/lesson05_task1.py
@@ -1,6 +1,19 @@
-from selenium import webdriver
 from time import sleep
-from selenium.webdriver.chr
-driver = webdriver.Chrome()
-driver.get("http://uitestingplayground.com/classattr")
+from selenium import webdriver
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+
+service = ChromeService(ChromeDriverManager().install())
+browser = webdriver.Chrome(service=service)
+
+
+browser.get('http://uitestingplayground.com/classattr')
+
+button = browser.find_element(By.CLASS_NAME, 'btn-primary')
+
+button.click()
 sleep(15)
