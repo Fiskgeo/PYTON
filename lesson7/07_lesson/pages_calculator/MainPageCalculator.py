af9486e220cc6e1b9b3add44f813c49239a2bc35
Fiskgeo <rncrus@gmail.com>
2025-11-03 14:52:12 +0300

    07-10-25

---
+++ lesson7/07_lesson/pages_calculator/MainPageCalculator.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+driver = None
+
+
+
+
+
+class MainPageCalculator:
+
+    def __init__(self, driver):
+        self._driver = driver
+
