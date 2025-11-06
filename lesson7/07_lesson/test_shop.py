d7e662b5ce8db83448f28a12ed21fcb69a274469
Fiskgeo <rncrus@gmail.com>
2025-11-06 06:27:10 +0300

    07-10-25

--- lesson7/07_lesson/test_shop.py
+++ lesson7/07_lesson/test_shop.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from pages_shop.MainPageShop import MainPage
 from pages_shop.LoginPage import LoginPage
@@ -7,9 +8,13 @@ from pages_shop.CartPage import CartPage
 def test_shop_buying():
     browser = webdriver.Chrome()
     main_page = MainPage(browser)
-    login_on_shop = LoginPage()
-    cart_adding_goods = CartAddingPage
+    login_page = LoginPage()
+    login_page.login_page(browser)
+    cart_adding = CartAddingPage()
+    cart_adding.cart_adding(browser)
+    time.sleep(8)
 
 
-cart_content = CartPage.get_cart_content()
-assert cart_content == "Ожидаемое содержимое"
+
+#cart_content = CartPage.get_cart_content(self)
+#assert cart_content == "Ожидаемое содержимое"
