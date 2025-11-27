e160e62831cca862f32fe306b19b1bab3e296e3b
Fiskgeo <rncrus@gmail.com>
2025-11-27 16:40:09 +0300

    07-10-25

--- lesson9/09_lesson/1.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from DataBaseClass import StudentTable
-
-db_connection_string = \
-    "postgresql://postgres:YV1018sql@localhost:5432/python_db"
-
-db = create_engine(db_connection_string)
